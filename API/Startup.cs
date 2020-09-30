72b1adc97822d0e776c1b73cf35594dd65a51b08
Tito Venter <umhwebi@venter.com>
2020-09-30 13:48:58 -0400

    End of Section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -34,6 +34,7 @@ namespace API
             });
 
             services.AddControllers();
+            services.AddCors();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
@@ -53,6 +54,8 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+            // This must be placed between UseRouting and UseAuthorization
+            app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
             app.UseAuthorization();
 
