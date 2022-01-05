47e90057916dd7a60f78f071a29b001bd86765c1
Tito Venter <umhwebi@venter.com>
2022-01-05 11:13:52 -0500

    End of section 13

--- API/Interfaces/ITokenService.cs
+++ API/Interfaces/ITokenService.cs
@@ -1,12 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using API.Entities;
 
 namespace API.Interfaces
 {
-    public interface ITokenService {
+    public interface ITokenService
+    {
         string CreateToken(AppUser user);
     }
 }
