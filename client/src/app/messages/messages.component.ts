29728e3a3dcf283eb2ac3ca24093259aa8fc613c
Tito Venter <umhwebi@venter.com>
2022-01-12 12:18:01 -0500

    End of section 18

--- client/src/app/messages/messages.component.ts
+++ client/src/app/messages/messages.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+
+import { ConfirmService } from '../_services/confirm.service';
 import { Message } from '../_models/message';
-import { Pagination } from '../_models/pagination';
 import { MessageService } from '../_services/message.service';
+import { Pagination } from '../_models/pagination';
 
 @Component({
   selector: 'app-messages',
@@ -16,7 +18,7 @@ export class MessagesComponent implements OnInit {
   pageSize = 5;
   loading = false;
 
-  constructor(private messageService: MessageService) { }
+  constructor(private messageService: MessageService, private confirmService: ConfirmService) { }
 
   ngOnInit(): void {
     this.loadMessages();
@@ -32,9 +34,14 @@ export class MessagesComponent implements OnInit {
   }
 
   deleteMessage(id: number) {
-    this.messageService.deleteMessage(id).subscribe(() => {
-      this.messages.splice(this.messages.findIndex(m => m.id === id), 1);
+    this.confirmService.confirm('Confirm delete message', 'This cannot be undone').subscribe(result => {
+      if (result) {
+        this.messageService.deleteMessage(id).subscribe(() => {
+          this.messages.splice(this.messages.findIndex(m => m.id === id), 1);
+        })
+      }
     })
+
   }
 
   pageChanged(event: any) {
